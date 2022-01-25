55e22d9b413d281eaf8acbfbb05d455bca926917
ShadityZ <68839412+ShadityZ@users.noreply.github.com>
2022-01-25 16:20:15 +1100

    Update setup.sh
    Fix that it opens in current terminal, so now it opens in a GUI terminal instead

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ FLAG="/var/log/firstboot.log"
 
 if [ ! -f $FLAG ]; then
    echo "This is the first boot"
-   python3 setup.py
+   x-terminal-emulator -e python3 setup.py
    touch $FLAG
    echo "Rebooting..."
    reboot
